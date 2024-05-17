dd6b15fb8542ab310b7fce728013e41b3585042e
DrDuckson <gorealex95@web.de>
2024-05-17 15:26:18 +0200

    Lua warmup

--- Main.lua
+++ Main.lua
@@ -1,2 +1,28 @@
 -- Text Adventure
-io.write("hello")
+
+local einMensch = {
+    -- meinAlter = 28,
+    -- meinName = "Alex",
+    -- meinBeruf = "Programmierer"
+    28,
+    "Alex",
+    "Programmierer"
+}
+
+print(einMensch[1])
+print(einMensch[2])
+print(einMensch[3])
+
+table.insert(einMensch, 4, "Test")
+
+for i, einMensch in ipairs(einMensch) do
+    print(i, einMensch)
+end
+
+local function greet(name)
+    print("Hallo, " .. name .. "!")
+end
+greet("Mom")
+
+table.remove(einMensch, 2) --rotz
+print(einMensch[2])
